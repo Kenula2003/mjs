b3fab48fb832b95e1daed002eabcb0a18f8e08fd
Kenula2003 <75131621+Kenula2003@users.noreply.github.com>
2024-09-13 17:58:15 +0200

    Create app.mjs

---
+++ app.mjs
@@ -0,0 +1,7 @@
+import addNumbers from './mathOperations.mjs';
+
+const num1 = 5;
+const num2 = 6;
+const result = addNumbers(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is: ${result}`);
